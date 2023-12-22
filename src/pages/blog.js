ea113d1614022390512ab0dfb03a37b7d7694654
giorgileladze <giorgileladze78@gmail.com>
2023-12-22 23:03:59 +0400

    implement blog info fetching

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,46 @@
+import { useEffect, useState } from "react";
+import api from "../axios";
+import { useNavigate, useParams } from "react-router-dom";
+import Blog from "../components/global/Blog/Blog.component";
+import goBackArrow from "../assets/icons/go-back-arrow.png"
+import BlogsList from "../components/global/BlogsList";
+
 const BlogPage = () => {
+    const [blog, setBlog] = useState(null);
+    const { blogId } = useParams();
+    const navigate = useNavigate();
+
+    const redirectToHome = () => {
+        navigate("/");
+    }
+
+    useEffect(() => {
+        const fetchBlogData = async () => {
+            try {
+                const response = await api.get(`/blogs/${blogId}`);
+                setBlog(response.data);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        if (blogId) {
+            fetchBlogData();
+        }
+    }, [blogId]);
+
     return (
-        <h1>blog</h1>
+        <>
+            <div className="pt-[40px]">
+                <img onClick={redirectToHome} src={goBackArrow} alt="goBackArrow" className="cursor-pointer"/>
+            </div>
+            <div>
+                <Blog blog={blog} />
+                <div className="mt-[98px]">
+                    <BlogsList categorieIds={[]} type="slider"/>
+                </div>
+            </div>
+        </>
     );
 }
 
